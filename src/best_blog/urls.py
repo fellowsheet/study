c8ee81c53204b3d7bfa514f3eccfb25cb197afb1
slavakyrlan <slavakyrlan00@mail.ru>
2024-01-25 13:12:49 +0300

    Added: login

--- src/best_blog/urls.py
+++ src/best_blog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from .views import APIAuthor
 
 urlpatterns = [
     path('authors/', APIAuthor.as_view()),
-    path('authors/<int:pk>/', APIAuthor.as_view())
+    path('authors/<int:pk>/', APIAuthor.as_view()),
+    path('accounts/', include(('accounts.urls', 'accounts'))),
 ]
