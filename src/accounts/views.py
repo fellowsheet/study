0aeb57418c210bd6f5cb47ba08a0cce3a98c7428
slavakyrlan <slavakyrlan00@mail.ru>
2024-01-25 18:46:31 +0300

    Added models: login, logout

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
 
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('api')  # заменить
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
 
+def user_logout(request):
+    logout(request)
+    return render(request, 'logout.html')
