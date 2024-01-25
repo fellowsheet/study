226e3211c11ecde14166964a359c9507c8b90ef8
slavakyrlan <slavakyrlan00@mail.ru>
2024-01-25 18:54:02 +0300

    Added models: in login revere

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.urls import reverse
+
 
 def user_login(request):
     if request.method == 'POST':
@@ -8,7 +10,7 @@ def user_login(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('api')  # заменить
+            return redirect(reverse('schema'))  # заменить
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
